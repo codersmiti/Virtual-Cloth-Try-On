d2dfc34e5163bc1b11c44f416084e1e7cdfd872b
Jayneel Shah <80264736+jayneel-shah18@users.noreply.github.com>
2024-02-22 17:29:20 +0530

    Update data_download.py

--- data_download.py
+++ data_download.py
@@ -2,10 +2,7 @@ import os
 import tarfile
 import shutil
 
-tarfile.open("data/viton_resize.tar.gz").extractall(path='data/')
+tarfile.open("/content/Virtual-Cloth-Try-On/data/viton_resize.tar.gz").extractall(path='/content/Virtual-Cloth-Try-On/data/')
 
 shutil.move('data/viton_resize/test/', 'data/test/')
-shutil.move('data/viton_resize/train/', 'data/train/')
-
-os.rmdir('data/viton_resize/')
-os.remove('data/viton_resize.tar.gz')
+shutil.move('data/viton_resize/train/', 'data/train/')
