1a27450f4c16ae35245163785c6cf2f5f8fc8a9d
Smiti Kothari <kotharismiti24@gmail.com>
2024-02-22 22:25:40 +0530

    Update data_download.py

--- data_download.py
+++ data_download.py
@@ -4,5 +4,5 @@ import shutil
 
 tarfile.open("/content/Virtual-Cloth-Try-On/data/viton_resize.tar.gz").extractall(path='/content/Virtual-Cloth-Try-On/data/')
 
-shutil.move('data/viton_resize/test/', 'data/test/')
-shutil.move('data/viton_resize/train/', 'data/train/')
+shutil.move('/content/Virtual-Cloth-Try-On/data/viton_resize/test/', '/content/Virtual-Cloth-Try-On/data/test/')
+shutil.move('/content/Virtual-Cloth-Try-On/data/viton_resize/train/','/content/Virtual-Cloth-Try-On/data/train/')
